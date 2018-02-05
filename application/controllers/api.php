a5765383fa285b31afd38a85f79e3b7cca291933
tsouhaieb <tarhounisouhaieb@gmail.com>
2018-02-05 21:43:16 +0100

    first commit

---
+++ application/controllers/api.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'/libraries/REST_controller.php';
+
+class Api extends REST_Controller {
+	function __construct() {
+		parent::__construct();
+	}
+
+	function student_get() {
+		$id = $this->uri->segment(3);
+		$student = array(
+			1 => array('first' => 'Jim', 'last_name' => 'Test'),
+			2 => array('first' => 'Jane', 'last_name' => 'Doe')
+		);
+		if (isset($student[$id])) {
+			$this->response(array('status' => 'success', 'message' => $student[$id]));
+		}
+		else {
+			$this->response(array('status' => 'failure', 'message' => 'The specified student could not be found'));
+		}
+	}
+}
