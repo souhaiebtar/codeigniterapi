10d2e44fcc1e6c65085f9c8cea1b77cc44dbd79b
tsouhaieb <tarhounisouhaieb@gmail.com>
2018-02-18 14:00:56 +0100

    add function to get all students

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -7,12 +7,19 @@ class Api extends REST_Controller {
 		parent::__construct();
 	}
 
+	private function _array_student() {
+        $student = array(
+            1 => array('first' => 'Jim', 'last_name' => 'Test'),
+            2 => array('first' => 'Jane', 'last_name' => 'Doe'),
+            3 => array('nom' => 'sss', 'prenom' => 'ss_prenom')
+        );
+
+	    return $student;
+
+    }
 	function student_get() {
 		$id = $this->uri->segment(3);
-		$student = array(
-			1 => array('first' => 'Jim', 'last_name' => 'Test'),
-			2 => array('first' => 'Jane', 'last_name' => 'Doe')
-		);
+		$student = $this->_array_student();
 		if (isset($student[$id])) {
 			$this->response(array('status' => 'success', 'message' => $student[$id]));
 		}
@@ -20,4 +27,27 @@ class Api extends REST_Controller {
 			$this->response(array('status' => 'failure', 'message' => 'The specified student could not be found'), REST_Controller::HTTP_CONFLICT);
 		}
 	}
+
+    function students_get() {
+        $student = $this->_array_student();
+        if (isset($student)) {
+            $this->response(array('status' => 'success', 'message' => $student));
+        }
+        else {
+            $this->response(array('status' => 'failure', 'message' => 'The specified student could not be found'), REST_Controller::HTTP_CONFLICT);
+        }
+    }
+
+	function professor_get() {
+	    $id = $this->uri->segment(3);
+	    $professor = array(
+	        1 => array('ss' => 'sd', 's2' => 's3'),
+            2 => array('sb' => 's1', 'ss1' => 's3')
+        );
+	    if (isset($professor[$id])) {
+	        $this->response(array('status' => 'success', 'message' => $professor[$id]));
+        } else {
+            $this->response(array('status' => 'failure', 'message' => 'The specified student could not be found'), REST_Controller::HTTP_CONFLICT);
+        }
+    }
 }
